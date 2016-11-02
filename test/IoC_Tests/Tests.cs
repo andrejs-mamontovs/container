d04b2da8e6ac198360d5b496bb879914e20a2f6e
Andrey Mamontov <andrey.mamontov@ctco.lv>
2016-11-02 22:32:39 +0200

    Unittest improvement * Cover scoped; * Cover singleton;

--- test/IoC_Tests/Tests.cs
+++ test/IoC_Tests/Tests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
 using IoC;
 
 namespace Tests
@@ -7,18 +6,88 @@ namespace Tests
     public class Tests
     {
         [Fact]
-        public void InitContainerTest() 
+        public void InitContainerTest()
         {
             var container = new Container();
-            
-            Assert.Same(container, 
-                container.AddSingleton<ServiceMock>());
 
-            Assert.True(true);
+            Assert.Same(container,
+                container.AddSingleton<ServiceMock>());
+            Assert.NotNull(container.Resolve<ServiceMock>());
+        }
+
+        [Fact]
+        public void NullSafeTest()
+        {
+            Container container = null;
+            Assert.Null(container.AddSingleton<ServiceMock>());
+            Assert.Null(container.AddScoped<ServiceMock>());
+            Assert.Null(container.AddSingleton<IServiceMock, ServiceMock>());
+            Assert.Null(container.AddScoped<IServiceMock, ServiceMock>());
+        }
+
+        [Fact]
+        public void SingletonTest()
+        {
+            var container = CreateContainerIncludeSingleton();
+
+            var expected =
+                container.Resolve<IServiceMock>();
+            Assert.Same(expected, container.Resolve<IServiceMock>());
+            Assert.Same(expected, container.Resolve<IServiceMock>());
+            Assert.Same(expected, container.Resolve<IServiceMock>());
+
+            var scope = container.Resolve<ScopeMock>();
+            Assert.Same(expected, scope.ServiceA);
+            Assert.Same(expected, scope.ServiceB);
+            Assert.Same(scope.ServiceA, scope.ServiceB);
+        }
+
+        [Fact]
+        public void ScopedTest()
+        {
+            var container = CreateContainerIncludeScoped();
+            var scopeA = container.Resolve<ScopeMock>();
+            var scopeB = container.Resolve<ScopeMock>();
+            Assert.Same(scopeA.ServiceA, scopeA.ServiceB);
+            Assert.Same(scopeB.ServiceA, scopeB.ServiceB);
+
+            Assert.NotSame(scopeA.ServiceA, scopeB.ServiceA);
+            Assert.NotSame(scopeA.ServiceB, scopeB.ServiceB);
+        }
+
+        private Container CreateContainerIncludeSingleton()
+        {
+            return new Container().AddSingleton<IServiceMock, ServiceMock>().Register<ScopeMock>();
+        }
+
+        private Container CreateContainerIncludeScoped()
+        {
+            return new Container().AddScoped<IServiceMock, ServiceMock>().Register<ScopeMock>();
         }
     }
 
-    public class ServiceMock {
+    public class ServiceMock : IServiceMock
+    {
+        public int Id
+        {
+            get; set;
+        }
+    }
 
+    public class ScopeMock
+    {
+
+        public IServiceMock ServiceA { get; }
+        public IServiceMock ServiceB { get; }
+        public ScopeMock(IServiceMock serviceA, IServiceMock serviceB)
+        {
+            this.ServiceA = serviceA;
+            this.ServiceB = serviceB;
+        }
+    }
+
+    public interface IServiceMock
+    {
+        int Id { get; set; }
     }
 }
